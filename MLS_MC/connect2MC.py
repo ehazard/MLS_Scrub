757c4f4e5e0bd618ddcf91216b12dad0adffd15f
Eric <erichazard49@gmail.com>
2017-10-22 13:51:48 -0400

    starting MLS_Matchcenter method

---
+++ MLS_MC/connect2MC.py
@@ -0,0 +1,13 @@
+import urllib
+from bs4 import BeautifulSoup
+
+url = "https://www.timbers.com/schedule?month=all&year=2017&club_options=7&op=Update&form_build_id=form-ztMv4lNCHH_kAAjTbGPY6E2NykGGbBKZyJ-r1d1_0f4&form_id=mp7_schedule_hub_search_filters_form"
+gross = urllib.urlopen(url)
+lol = gross.read()
+
+soup = BeautifulSoup(lol) #HTML of the above web page is now in *soup*
+
+matchDates = soup.findAll("div", { "class": "match_date"})
+
+for i in matchDates:
+    print i
