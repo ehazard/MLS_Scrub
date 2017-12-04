a410f86f8eb4e38ce27cbfc6d81ed4b86573bc7c
Eric <erichazard49@gmail.com>
2017-12-04 10:28:15 -0500

    fixed a URL positioning issue

--- flask/myApp/myApp.py
+++ flask/myApp/myApp.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template,request
-import json
+from flask import Flask, render_template,request,url_for
+import json, os
 import connect2MC as connect #connect.basetHTML()
 app = Flask(__name__)
 
@@ -15,7 +15,8 @@ def hello_world():
 def subs_stats():
     if request.method == 'POST':
         team = request.form['teamN']
-        jsonF = open('teams.json').read()
-        newDict = json.loads(jsonF) #newDict['SEA']
+        filename = os.path.join(app.static_folder, 'teams.json')
+        with open(filename) as jsonFile:
+            newDict = json.load(jsonFile)
         subList = connect.baseHTML(newDict[team], team)
         return render_template('placeholder.html', team=team, subList=subList)
